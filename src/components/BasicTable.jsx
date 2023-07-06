91fdc3f3d925c4f219f7348490df693a3dc9dd10
kalaioryo <74815990+kalaioryo@users.noreply.github.com>
2023-07-06 17:30:12 +0200

    edited Table

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -1,70 +1,53 @@
-import React, {useMemo} from 'react'
+import React, { useMemo } from "react";
 
-import { useTable } from 'react-table'
+import { useTable } from "react-table";
 
-const BasicTable = ({dataTable}) => {
+const BasicTable = ({ dataTable, columnsData }) => {
+  const data = useMemo(() => dataTable, []);
+  const columns = useMemo(() => columnsData, []);
 
-  // const fakeData = [
-  //   {
-  //     denominazione_provincia: "Vercelli",
-  //     totale_casi: 23000
-  //   },
-  //   {
-  //     denominazione_provincia: "Biella",
-  //     totale_casi: 16000
-  //   },
-  // ]
-  
-
-  const data = useMemo(() => dataTable, [])
-  const columns = useMemo(() => [
-    {
-      Header: "Name",
-      accessor: "denominazione_provincia"
-    },
-    {
-      Header: "Tot case",
-      accessor: "totale_casi"
-    },
-  ], [])
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({columns, data})
-
-  
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
 
   return (
-    <div className=' mt-14 border border-red-500'>
-        <div className='col-span-10 col-start-2'>
-          <table {...getTableProps()}>
-            <thead>
-              {headerGroups.map((headerGroup) => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => (
-                    <th {...column.getHeaderProps()}>
-                      {column.render("Header")}
-                    </th>
+    <div className="grid grid-cols-12 col-span-10 col-start-2 lg:col-span-6 lg:col-start-4">
+      <div className="col-span-12 bg-quaternary/50">
+        <table {...getTableProps()} className="w-full ">
+          <thead className="border bg-ternary/50">
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th
+                    {...column.getHeaderProps()}
+                    className="border border-primary"
+                  >
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()} className="border border-primary">
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td
+                      {...cell.getCellProps()}
+                      className="border border-primary p-2"
+                    >
+                      {cell.render("Cell")}
+                    </td>
                   ))}
                 </tr>
-              ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-              {rows.map((row) => {
-                prepareRow(row)
-                return(
-                  <tr {...row.getRowProps()}>
-                    {row.cells.map((cell) => (
-                      <td{ ...cell.getCellProps()}>
-                        {cell.render("Cell")}
-                      </td>
-                    ))}
-                  </tr>
-                )
-              })}            
-            </tbody>
-          </table>
-        </div>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default BasicTable
+export default BasicTable;
--- src/pages/Provinces.jsx
+++ src/pages/Provinces.jsx
@@ -13,6 +13,30 @@ const Provinces = () => {
 
   const { IsLoading, IsError, provincesLatest} = provinces
 
+  const dataTable = provincesLatest.map((province) => {
+    const provinceObject = {
+      denominazione_regione: province.denominazione_regione,
+      denominazione_provincia: province.denominazione_provincia,
+      totale_casi: province.totale_casi.toLocaleString('it-IT')
+    }
+    return provinceObject
+  })
+
+  const columnsData = [
+    {
+      Header: "Regione",
+      accessor: "denominazione_regione"
+    },
+    {
+      Header: "Provincia",
+      accessor: "denominazione_provincia"
+    },
+    {
+      Header: "Totale casi",
+      accessor: "totale_casi"
+    },
+  ]
+
 
 
   return (
@@ -25,8 +49,12 @@ const Provinces = () => {
       {
 
         !IsLoading && provincesLatest[0] ? 
+        <div className='grid grid-cols-12 bg-quaternary/60'>
+        <h1 className="col-span-12 py-6 text-3xl text-center">Province</h1>
+          <BasicTable dataTable={dataTable} columnsData={columnsData}/>
+        </div>
 
-        <BasicTable dataTable={provincesLatest} />
+        
 
       : null
       }
