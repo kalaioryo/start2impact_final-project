10e95aca6f476d7d0e178a78d58794e6f337f303
kalaioryo <kalaioryo@gmail.com>
2023-07-17 19:19:24 +0200

    snd props lottie

--- src/components/lottie-components/LottieDarkMode.jsx
+++ src/components/lottie-components/LottieDarkMode.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import Lottie from "lottie-react";
 
 import darkModeAnimation from "../../assets/lotties/dark-mode-animation.json";
 
 const LottieDarkMode = () => {
+  const [ isDark, setIsDark ] = useState(false)
+
+  const lottieRef = useRef();
+
+
   const style = {
     width: "45px",
     margin: "5px 20px 0 0",
-    padding: "1px"
+    padding: "1px",
     // backgroundColor: 'red'
   };
 
+  const handleClick = () => {
+    // setIsDark(!isDark)
+    // lottieRef.current.destroy()
+    console.log(isDark);
+  }
+
   return (
-    <div className=''>
-      <Lottie animationData={darkModeAnimation} style={style} />
+    <div className="">
+      <Lottie
+        onClick={handleClick}
+        animationData={darkModeAnimation} 
+        style={style} 
+        lottieRef={lottieRef}
+        loop={false}
+        
+        />
     </div>
   );
 };
