2e3296c17be9eb398131833310c68f9e601e3d11
kalaioryo <74815990+kalaioryo@users.noreply.github.com>
2023-07-22 20:54:53 +0200

    changed color button

--- src/components/button/SelectCurrentKey.jsx
+++ src/components/button/SelectCurrentKey.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const SelectCurrentKey = ({handleClickCurrentKey, text, keyData}) => {
   return (
       <button
-        className="bg-red-400 p-4 m-1"
+        className="p-4 m-1 rounded-md bg-ternary/60 text-primary hover:text-quaternary active:hover:bg-ternary/20 active:hover:text-primary hover:bg-primary/60 
+        dark:bg-dark-secondary dark:text-dark-quaternary dark:hover:bg-dark-ternary dark:hover:text-dark-primary dark:active:hover:bg-dark-ternary/20 dark:active:hover:text-dark-quaternary"
         onClick={() => handleClickCurrentKey([keyData])}
       >
         {text}
--- src/components/button/SelectTimeRange.jsx
+++ src/components/button/SelectTimeRange.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const SelectTimeRange = ({text, setTimeRange, timeRange}) => {
-
-
+const SelectTimeRange = ({ text, setTimeRange, timeRange }) => {
   return (
-    <div className='col-span-2'>
-      <button className='bg-red-300 p-4 m-1' onClick={() => setTimeRange(timeRange)}>
+    <div className="col-span-2">
+      <button
+        className="rounded-lg p-4 m-1 bg-ternary/60 text-primary hover:text-quaternary active:hover:bg-ternary/20 active:hover:text-primary hover:bg-primary/60 
+        dark:bg-dark-secondary dark:text-dark-quaternary dark:hover:bg-dark-ternary dark:hover:text-dark-primary dark:active:hover:bg-dark-ternary/20 dark:active:hover:text-dark-quaternary"
+        onClick={() => setTimeRange(timeRange)}
+      >
         {text}
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default SelectTimeRange
+export default SelectTimeRange;
