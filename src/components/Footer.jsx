15fa11e479d7a18eb6003dbea35c1bd6642c79f8
kalaioryo <74815990+kalaioryo@users.noreply.github.com>
2023-07-24 16:04:16 +0200

    fix z-index footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='fixed inset-x-0 bottom-0 text-center w-full bg-secondary text-quinary py-6 dark:bg-dark-primary dark:text-dark-ternary'>
+    <footer className='fixed inset-x-0 bottom-0 text-center w-full z-20  bg-secondary text-quinary py-6 dark:bg-dark-primary dark:text-dark-ternary'>
       <h1>Made dy Antonio Iorio whit love in 2023</h1>
     </footer>
   )
