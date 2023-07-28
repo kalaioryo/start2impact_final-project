4cc1f7d5a0a979245a10a71883c6d19befeefa1a
kalaioryo <74815990+kalaioryo@users.noreply.github.com>
2023-07-28 13:41:42 +0200

    Added favicon chang title. Deleted App.js and App.css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import { useDispatch } from 'react-redux';
-// import { fetchDataRegionsLatest } from './features/regionsLatest/regionsLatestSlice';
-// import { fetchDataItaly } from './features/italy/italySlice';
-// import { fetchDataRegions } from './features/regions/regionsSlice';
-
-
 import store from './store/store';
 
-import App from './App';
 import { Provider } from 'react-redux';
 
 import { RouterProvider } from 'react-router-dom';
@@ -17,17 +10,11 @@ import { router } from './routes';
 
 import './index.css';
 
-// dispatch(fetchDataRegionsLatest())
-// dispatch(fetchDataItaly())
-// dispatch(fetchDataRegions())
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router}/>
-      {/* <App /> */}
     </Provider>
     
   </React.StrictMode>
